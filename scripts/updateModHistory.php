ad3eb146f3c8e679f27f07d8ca181a5ba5cf7ad7
haszi <haszika80@gmail.com>
2024-07-21 11:34:17 +0200

    Use BASE_SHA for git diff

--- scripts/updateModHistory.php
+++ scripts/updateModHistory.php
@@ -45,13 +45,13 @@ echo `echo \$GITHUB_BASE_SHA`;
 echo `echo \$GITHUB_REF`;
 echo `echo \$GITHUB_SHA`;
 echo `echo git diff --name-only \$GITHUB_BASE_REF \$GITHUB_SHA`;
-echo `git diff --name-only \$GITHUB_BASE_REF \$GITHUB_SHA`;
+echo `git diff --name-only \$GITHUB_BASE_SHA \$GITHUB_SHA`;
 
 $modifiedFilescommand = <<<COMMAND
 #!/usr/bin/env bash
 echo "last commit hash:"
 echo "$(git rev-parse HEAD)"
-git diff --name-only \$GITHUB_BASE_REF \$GITHUB_SHA | while read -r filename; do
+git diff --name-only \$GITHUB_BASE_SHA \$GITHUB_SHA | while read -r filename; do
   echo "filename:"
   echo "\$filename"
   echo "modified:"
