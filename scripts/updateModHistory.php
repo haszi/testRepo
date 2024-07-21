62762c29904bb51769de9e94200d9d4722cb8a0a
haszi <haszika80@gmail.com>
2024-07-21 11:23:35 +0200

    a

--- scripts/updateModHistory.php
+++ scripts/updateModHistory.php
@@ -41,17 +41,16 @@ if (isset($modHistoryArray["last commit hash"]) && $modHistoryArray["last commit
 
 echo "Github Vars: ";
 echo `echo \$GITHUB_BASE_REF`;
-echo `echo \$GITHUB_BASE_SHA`;
 echo `echo \$GITHUB_REF`;
-echo `echo \$GITHUB_REF_NAME`;
 echo `echo \$GITHUB_SHA`;
-echo `echo git diff --name-only \$GITHUB_BASE_SHA \$GITHUB_SHA`;
+echo `echo git diff --name-only \$GITHUB_BASE_REF \$GITHUB_SHA`;
+echo `git diff --name-only \$GITHUB_BASE_REF \$GITHUB_SHA`;
 
 $modifiedFilescommand = <<<COMMAND
 #!/usr/bin/env bash
 echo "last commit hash:"
 echo "$(git rev-parse HEAD)"
-git diff --name-only \$GITHUB_BASE_SHA \$GITHUB_SHA | while read -r filename; do
+git diff --name-only \$GITHUB_BASE_REF \$GITHUB_SHA | while read -r filename; do
   echo "filename:"
   echo "\$filename"
   echo "modified:"
